8a1ce7f76f7264e8c2026bffe2e40bb38cb1d707
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-18 18:24:06 -0400

    added route for Get Profile by User Id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,4 +102,36 @@ router.post(
   }
 );
 
+// @route GET api/profile || @desc Get all Profiles || @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route GET api/profile/profile/user/:user_id || @desc Get all Profiles || @access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile Not Found' });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+        return res.status(400).json({ msg: 'Profile Not Found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;

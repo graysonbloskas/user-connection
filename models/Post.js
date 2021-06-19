5dc61d5d93e62988b50fa97839a9fa8c0279b83a
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-19 00:06:56 -0400

    created Post model

--- models/Post.js
+++ models/Post.js
@@ -15,29 +15,42 @@ const PostSchema = new Schema({
     type: String,
   },
   avatar: {
-      type: String,
+    type: String,
   },
   likes: [
-      {
-          user: {
-              type: Schema.Types.ObjectId,
-              ref: 'users',
-          }
-      }
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
   ],
   comments: [
-      {
-          user: {
-              type: Schema.Types.ObjectId,
-              ref: 'users',
-          },
-          text: {
-              type: String,
-              required: true,
-          },
-          name: {
-              type: String,
-          }
-      }
-  ]
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = Post = mongoose.model('post', Post);

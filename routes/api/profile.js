6e61b8f0aeaa7aaf63403f2e8d93267596a0cb29
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-18 23:12:38 -0400

    created route for deleting profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -167,7 +167,8 @@ router.put(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { title, company, from, to, current, description, location } = req.body;
+    const { title, company, from, to, current, description, location } =
+      req.body;
 
     const newExp = {
       title,
@@ -192,5 +193,25 @@ router.put(
     }
   }
 );
+
+// @route DELETE api/profile/experience/:exp_id || @desc Delete experience from profile  || @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;

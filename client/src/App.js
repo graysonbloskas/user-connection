2dbc2e1d49adf33f99b82eda0eadcc7db41e7590
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-19 21:02:18 -0400

    created redux store, created actions, types, reducers, so alerts can now be set and called

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,27 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+
+// Bring in redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router>
+    <Provider store={store}>
+      <Router>
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
           </Switch>
         </section>
-    </Router>
+      </Router>
+    </Provider>
   );
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,9 +21,9 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords must match');
+      setAlert('Passwords must match', 'danger');
     } else {
-      console.log('Success')
+      console.log('Success');
     }
   };
   return (
@@ -83,4 +86,7 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+export default connect(null, { setAlert })(Register);

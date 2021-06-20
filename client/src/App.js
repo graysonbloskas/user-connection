3a820327ac5cf2551e3b2d756c9f030b2dacd716
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-19 20:00:28 -0400

    created login and register components, suse state hooks for data entered in form

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
-    <Fragment>
-       <Navbar />
-       <Landing />
-    </Fragment>
+    <Router>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+    </Router>
   );
-}
+};
 
 export default App;

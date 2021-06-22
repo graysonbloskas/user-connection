45b4bbdd03e4f1d8089099934851ef10cb81e20d
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-22 15:07:09 -0400

    created auth actions for axios call to register user, set register user onSubmit of register form, redux dev tools is storing token and payload in local storage

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -23,7 +24,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords must match', 'danger');
     } else {
-      console.log('Success');
+      register({ name, email, password });
     }
   };
   return (
@@ -40,7 +41,7 @@ const Register = ({ setAlert }) => {
             name='name'
             value={name}
             onChange={(e) => onChange(e)}
-            required
+            
           />
         </div>
         <div className='form-group'>
@@ -50,7 +51,7 @@ const Register = ({ setAlert }) => {
             name='email'
             value={email}
             onChange={(e) => onChange(e)}
-            required
+            
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -88,5 +89,6 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);

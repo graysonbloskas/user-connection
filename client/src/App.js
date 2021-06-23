657a4626e3a0296c44fa034c41b8d692fa56f528
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-23 16:05:09 -0400

    added private route, redirects un authorized person to login page

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/private/PrivateRoute';
 
 // Bring in redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken'
+import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -20,7 +22,7 @@ if (localStorage.token) {
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
-  }, [])
+  }, []);
 
   return (
     <Provider store={store}>
@@ -32,6 +34,7 @@ const App = () => {
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
           </Switch>
         </section>
       </Router>

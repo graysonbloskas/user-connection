619e212ca5016c6f0c6bf93c3c0b31b62648d486
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-23 17:49:41 -0400

    added logic for dash board, getting current name, added spinner gif

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import spinner from './spinner.gif';
+
+export default () =>  (
+    <>
+        <img src={spinner} style={{ width: '30%', margin: 'auto', display: 'block' }} alt="spinner loading gif" />
+    </>
+)

74cadb732d349496803998a92f1c27106bf8fda9
Grayson Bloskas <graysonbloskas@gmail.com>
2021-06-30 18:05:25 -0400

    created experience section and education section to user profiles

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout'
+import ProfileExerience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({
   getProfileById,
@@ -36,7 +38,22 @@ const Profile = ({
             <div className="profile-grid my-1">
                 <ProfileTop profile={profile} />
                 <ProfileAbout profile={profile} />
+                <div className="profile-exp bg-white p-2">
+                    <h2 className='text-primary'>Experience</h2>
+                    {profile.experience.length > 0 ? (<> {profile.experience.map(experience => (
+                        <ProfileExerience key={experience._id} experience={experience} />
+                    ))} </>) : (<h4>No Experience Added</h4>)}
 
+                    
+                </div>
+                <div className="profile-edu bg-white p-2">
+                    <h2 className='text-primary'>Education</h2>
+                    {profile.education.length > 0 ? (<> {profile.education.map(education => (
+                        <ProfileEducation key={education._id} education={education} />
+                    ))} </>) : (<h4>No Education Added</h4>)}
+
+                    
+                </div>
             </div>
         </>
       )}

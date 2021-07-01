964008dd52b09f41e5bc73ff07da01728c1e18b6
Grayson Bloskas <graysonbloskas@gmail.com>
2021-07-01 17:24:10 -0400

    added like and unlike action, reducer, and functionality to posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
-export const getPosts = () => async dispatch => {
+export const getPosts = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/posts');
 
@@ -11,7 +11,35 @@ export const getPosts = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: POST_ERROR,
-        payload: { msg: err.response.statusText, status: err.response.status },
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Like
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({ type: UPDATE_LIKES, payload: { id, likes: res.data } });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Remove Likes
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+
+    dispatch({ type: UPDATE_LIKES, payload: { id, likes: res.data } });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
 };

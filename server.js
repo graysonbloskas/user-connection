d4c58b4b8fbe7f4d2da940664ebbf3557944d22d
Grayson Bloskas <graysonbloskas@gmail.com>
2021-07-01 23:58:10 -0400

    fix deploy take 3

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-} 
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 

545d2c5b360ef0475d8d86b7106618d4ddae06a2
Grayson Bloskas <graysonbloskas@gmail.com>
2021-07-02 00:00:42 -0400

    fix deploy take 4

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
     });
   }
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Server Started", this.address().port, app.settings.env);
+  });

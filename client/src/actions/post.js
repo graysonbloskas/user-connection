05025a67afda4ce06336f99a813bf5c9ac4971dd
Grayson Bloskas <graysonbloskas@gmail.com>
2021-07-01 20:26:51 -0400

    added comment section, user able to delete comments, the application is finished

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from './types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
+} from './types';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -61,12 +70,12 @@ export const deletePost = (id) => async (dispatch) => {
 };
 
 // Add Post
-export const addPost = formData => async (dispatch) => {
+export const addPost = (formData) => async (dispatch) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
-  }
+      'Content-Type': 'application/json',
+    },
+  };
 
   try {
     const res = await axios.post('/api/posts', formData, config);
@@ -83,11 +92,53 @@ export const addPost = formData => async (dispatch) => {
 };
 
 // Get post
-export const getPost = id => async (dispatch) => {
+export const getPost = (id) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/posts/${id}`);
 
     dispatch({ type: GET_POST, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Comment
+export const addComment = (postId, formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+
+    dispatch({ type: ADD_COMMENT, payload: res.data });
+
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({ type: REMOVE_COMMENT, payload: commentId });
+
+    dispatch(setAlert('Comment Deleted', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,8 @@ import {connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 import { Link } from 'react-router-dom';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -19,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       <>
       <Link to='/posts' className='btn'>Back to Posts</Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
+        <div className='comments'>
+          {post.comments.map(comment => (
+            <CommentItem key={comment._id} comment={comment} postId={post._id} />
+          ))}
+        </div>
       </>
     ));
   };
